32b65aa6d6a8d2e4eca9deac04b626c662adf514
viniz15 <viniciusz1525@gmail.com>
2025-04-11 15:01:12 -0300

    Add files via upload

--- Login/index.php
+++ Login/index.php
@@ -3,14 +3,23 @@
 //importa a classe
 require 'Usuario.php';
 
+//variaveis
 $sucesso = $usuario = new Usuario();
 
+if(isset($_POST ['inserirNome']) && isset($_POST ['inserirEmail']) && isset($_POST ['inserirSenha'])){
+    $name = $_POST_['inserirNome'];
+    $email = $_POST_['inserirEmail'];
+    $pass = $_POST_['inserirSenha'];
+    $usuario = new Usuario();
+    $usuario-> cadastrar($name, $email, $pass);
+}
+
 if ( $sucesso ){   
-    $ff = $usuario->chkUser("viniciusz1525@gmail.com");
+    $ff = $usuario->chkUser($email);
 
     // se o usuario não exitir cadastre ele, se não imprima uma menssagem
     if( $ff == 0){
-        $usuario->cadastrar("Vinicius Zidomar", "viniciusz1525@gmail.com", "12345678");
+        $usuario->cadastrar($name, $email, $pass);
         echo "<h1>Cadastrado com sucesso!";
     }else{
         echo "<h1>O usuário já existe.";
